cee233c4280568741e1859158576852e6064a785
Muhammet Ali Amanvermez <64315684+aliamanvermez@users.noreply.github.com>
2021-06-21 01:39:17 +0300

    Add files via upload

---
+++ Ekip Mis/SQLQuery2.sql
@@ -0,0 +1,31 @@
+select * from PATIENTS
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (1,1,8,'Ahmet','Bayýndýr',28,'FG32-AS32-15','2021-05-20');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (2,1,8,'Ali','Sekman',29,'FG32-BS32-16','2021-11-21');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (3,1,8,'Beyza','Altýngöz',30,'FG32-CS32-17','2021-01-21');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (4,1,8,'Serdar','Kuzuloðlu',31,'FG32-DS32-18','2021-09-22');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (5,1,8,'Derya','Kendirci',32,'FG32-ES32-19','2021-08-23');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (1,2,9,'Ekrem','Duman',33,'FG32-FS32-20','2021-05-24');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (2,2,9,'Behzat','Çizer',34,'FG32-GS32-21','2021-01-25');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (3,2,9,'Asuman','Derdiyok',35,'FG32-HS32-22','2021-03-25');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (4,2,9,'Sema','Koyuncu',36,'FG32-HI32-23','2021-01-26');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (5,2,9,'Sedat','Teker',37,'FG32-JS32-14','2021-03-26');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (1,3,9,'Ersel','Kabirci',38,'FG32-KS32-14','2021-05-26');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (2,3,9,'Harun','Can',39,'FG32-LS32-14','2021-01-28');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (3,3,9,'Ýsmet','Akbaba',40,'FG32-MS32-14','2021-03-21');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (4,3,9,'Sabri','Hayalet',41,'FG32-NS32-14','2021-01-18');
+INSERT INTO dbo.PATIENTS ( variant_id,region_id , hospital_id ,patient_name ,patient_surname,wristband_id,hes_code,control_date)
+VALUES (5,3,9,'Eda','Ersoy',42,'FG32-OS32-14','2021-03-19');
